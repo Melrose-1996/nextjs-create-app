53ad770035512fef7bb02abc2c102bf25d78f8aa
Melrose <2713605684@qq.com>
2021-04-29 22:52:48 +0800

    feat:4/29

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Router from "next/router";
 import { Button } from "antd";
 
+import store from "../store/store";
+
 const events = [
   "routeChangeStart",
   "routeChangeComplete",
