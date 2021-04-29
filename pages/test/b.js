101dd56a55f20cd2051e68b790863800109d80e8
Melrose <2713605684@qq.com>
2021-04-30 01:32:08 +0800

    feat:commit

--- pages/test/b.js
+++ pages/test/b.js
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
 
+function countReducer(state, action) {}
+
 function MyCount() {
   const [count, setCount] = useState(0);
+  // 这里传入的什么，就会把 count 设置为这个值，这个值是不基于我们最新的 count
+  setCount(1);
+  // 这个 c 是我们调用的这个 setcount 最新的 count 值
+  setCount((c) => c + 1);
+
   useEffect(() => {
     const interval = setInterval(() => {
       setCount((c) => c + 1);
